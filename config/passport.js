0e315f7df91bce2c7592edb8054069dcc6c125e3
김정호 <starjhjh@naver.com>
2021-05-23 08:05:33 +0000

    console commit

--- config/passport.js
+++ config/passport.js
@@ -2,43 +2,47 @@
  * 패스포트 기본 설정 파일
  * 
  * 패스포트 설정을 위한 기본 파일로 passport 폴더에 있는 설정 파일들을 사용함
- * serializeUser, deserializeUser 메소드 설정
- *
- * @date 2016-11-10
- * @author Mike
+ * serializeUser, deserializeUser 메소드 설정은 생활코딩을 참고함.
  */
 
 var local_login = require('./passport/local_login');
 var local_signup = require('./passport/local_signup');
 
+
 module.exports = function (app, passport) {
 	console.log('config/passport 호출됨.');
+	
+	//이 부분 추후 손볼것. -05.23
+	var auth = [
+		{local_login: require('./passport/local_login')},
+		{local_signup: require('./passport/local_signup')}
+	];
 
-    // 사용자 인증 성공 시 호출
-    // 사용자 정보를 이용해 세션을 만듦
-    // 로그인 이후에 들어오는 요청은 deserializeUser 메소드 안에서 이 세션을 확인할 수 있음
-    passport.serializeUser(function(user, done) {
-        console.log('serializeUser() 호출됨.');
-        console.dir(user);
+	
+	//serializeUser 메서드는 로그인에 성공했을 때 전달한 authData 객체를 콜백 함수의 첫 번째 인자로 전달받는다.
+    passport.serializeUser( (user, done) => {
+        console.log('serializeUser', user);
+		
+		/* 
+		 * 전달받은 사용자 정보를 내부적으로 세션에 기록한다.
+		 * 세션에 기록하는 일은 done 함수가 수행.
+		 * done 함수의 첫 번째 인자로는 null, 두 번째 인자로는 사용자를 구별하는 식별자를 전달함. 
+		 */ 
+        done(null, user);  
+    }); 
 
-        done(null, user);  // 이 인증 콜백에서 넘겨주는 user 객체의 정보를 이용해 세션 생성
-    });
-
-    // 사용자 인증 이후 사용자 요청 시마다 호출
-    // user -> 사용자 인증 성공 시 serializeUser 메소드를 이용해 만들었던 세션 정보가 파라미터로 넘어온 것임
-    passport.deserializeUser(function(user, done) {
-        console.log('deserializeUser() 호출됨.');
-        console.dir(user);
-
-        // 사용자 정보 중 id나 email만 있는 경우 사용자 정보 조회 필요 - 여기에서는 user 객체 전체를 패스포트에서 관리
+    // deserializeUser 메서드는 페이지에 방문할 때마다 호출되게 약속돼있다.
+    // 사용자가 각 페이지에 방문할 때마다 세션에 기록된 데이터(user)를 기준으로 deserializeUser 메서드를 호출해 로그인 여부를 확인
+    passport.deserializeUser( (user, done) => {
+        console.log('deserializeUser', user);
+		
         // 두 번째 파라미터로 지정한 사용자 정보는 req.user 객체로 복원됨
-        // 여기에서는 파라미터로 받은 user를 별도로 처리하지 않고 그대로 넘겨줌
         done(null, user);  
     });
 
 	// 인증방식 설정
 	passport.use('local-login', local_login);
 	passport.use('local-signup', local_signup);
-	console.log('2가지 passport 인증방식 설정됨.');
+	console.log(auth.length,'가지 passport 인증방식 설정됨.');
 	
 };
